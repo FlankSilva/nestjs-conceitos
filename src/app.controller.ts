9186f145caaac47bceb007898d9de8e6326f98a4
Flank Silva <flank.silva.0@gmail.com>
2025-02-25 17:02:53 -0300

    implements prisma studio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
 
 @Controller('app')
 export class AppController {
+  constructor(
+    private prisma: PrismaService
+  ) {}
+
   @Get('hello')
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const member = await this.prisma.rocketTeamMember.create({
+data: {
+  id: 'capitao',
+  name: 'Flank Silva',
+  function: 'Capitão'
+}
+    })
   }
 }
