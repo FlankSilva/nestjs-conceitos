07ea90244a3a7ca21626d11f1046f11cb353a598
Flank Silva <flank.silva.0@gmail.com>
2025-02-25 17:08:35 -0300

    removed eslint and prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { PrismaService } from './database/prisma.service';
+import { Controller, Get } from "@nestjs/common";
+import { PrismaService } from "./database/prisma.service";
 
-@Controller('app')
+@Controller("app")
 export class AppController {
-  constructor(
-    private prisma: PrismaService
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get('hello')
+  @Get("hello")
   async getHello() {
     const member = await this.prisma.rocketTeamMember.create({
-data: {
-  id: 'capitao',
-  name: 'Flank Silva',
-  function: 'Capitão'
-}
-    })
+      data: {
+        id: "capitao2",
+        name: "Flank Silva 2",
+        function: "Capitão",
+      },
+    });
   }
 }
