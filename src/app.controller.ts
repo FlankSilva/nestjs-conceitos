b9c71b813c46efe27e9c470d0ad184b1643796ca
Flank Silva <flank.silva.0@gmail.com>
2025-02-25 21:00:55 -0300

    create new member route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
-import { PrismaService } from "./database/prisma.service";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateTeamMemberBody } from "./dtos/create-team-member-body";
+import { RocketMembersRepository } from "./repositories/rockert-members-repository";
 
 @Controller("app")
 export class AppController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private rocketMembersRepository: RocketMembersRepository) {}
+
+  @Post("hello")
+  async createHello(@Body() body: CreateTeamMemberBody) {
+    const { name, function: memberFunction } = body;
+
+    await this.rocketMembersRepository.create(name, memberFunction);
+  }
 
   @Get("hello")
-  async getHello() {
-    const member = await this.prisma.rocketTeamMember.create({
-      data: {
-        id: "capitao2",
-        name: "Flank Silva 2",
-        function: "Capitão",
-      },
-    });
-  }
+  async getAllMembers() {}
 }
